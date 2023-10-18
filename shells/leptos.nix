5569959c67a4fb887b0126485017e0acb485a318
Andreas Krath <andreas.krath+github@gmail.com>
2023-10-18 13:30:28 +0200

    add requires packages for leptos to work
    all the js/ts is only for end2end testing, and could in theory be omitted

--- shells/leptos.nix
+++ shells/leptos.nix
@@ -3,17 +3,30 @@ let
   rust = pkgs.rust-bin.selectLatestNightlyWith(toolchain: toolchain.default.override {
     extensions = [ "rust-src" ];
     targets = [ "wasm32-unknown-unknown" ];
-    });
+  });
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     rust
     vscode-extensions.rust-lang.rust-analyzer
     cargo-leptos
+    cargo-generate
+    trunk
+    tailwindcss
 
     # required for things like tokio
     pkg-config
     openssl
+
+    # basic js
+    nodejs
+    nodePackages.npm
+    nodePackages.prettier # formatter
+    nodePackages.eslint # linter
+
+    # typescript
+    nodePackages.typescript
+    nodePackages.typescript-language-server
   ];
 
   RUST_BACKTRACE = "1";
