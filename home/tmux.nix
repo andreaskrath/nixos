dd6fd45de5d46350217adbfc2895874d158c7654
Andreas Krath <andreas.krath+github@gmail.com>
2023-11-18 20:23:10 +0100

    home: set right modules to dir and ses

--- home/tmux.nix
+++ home/tmux.nix
@@ -35,18 +35,19 @@
           set -g @catppuccin_window_right_separator "█ "
           set -g @catppuccin_window_number_position "left"
           set -g @catppuccin_window_middle_separator " | "
-          
+
           set -g @catppuccin_window_default_fill "none"
-          
           set -g @catppuccin_window_default_text "#W"
+
           set -g @catppuccin_window_current_fill "all"
-          
-          set -g @catppuccin_status_modules_right "application session user host"
           set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_status_modules "directory session"
           set -g @catppuccin_status_left_separator "█"
           set -g @catppuccin_status_right_separator "█"
-          
-          set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M:%S"
+          set -g @catppuccin_directory_text "#{pane_current_path}"
+
+        '';
         '';
       }
     ];
