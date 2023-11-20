52dba71de71abcc683def793b0d52791515a9451
Andreas Krath <andreas.krath+github@gmail.com>
2023-11-20 23:55:04 +0100

    home: update hardcoded path to nixvariable

--- home/zsh/scripts/setup_env.nix
+++ home/zsh/scripts/setup_env.nix
@@ -1,4 +1,7 @@
 { pkgs }:
+let
+  basePath = "/etc/nixos/shells/";
+in
 pkgs.writeShellApplication {
   name = "setup_env";
   runtimeInputs = [];
@@ -7,22 +10,22 @@ pkgs.writeShellApplication {
     
     if [ -z "$1" ]; then
       echo "Please specify which nix-shell you wish to utilize. Available options are:"
-      for file in /etc/nixos/shells/*
+      for file in ${basePath}*
       do
         [[ ! -e "$file" ]] && break
-        stripped_prefix=''${file##/etc/nixos/shells/}
+        stripped_prefix=''${file##${basePath}}
         stripped_suffix=''${stripped_prefix%%.nix}
         echo "$stripped_suffix"
       done
       exit 1
     fi
 
-    if ! [ -f "/etc/nixos/shells/$1.nix" ]; then
+    if ! [ -f "${basePath}$1.nix" ]; then
       echo "The shell you specified '$1' is not a valid shell file. Available options are:"
-      for file in /etc/nixos/shells/*
+      for file in ${basePath}*
       do
         [[ ! -e "$file" ]] && break
-        stripped_prefix=''${file##/etc/nixos/shells/}
+        stripped_prefix=''${file##${basePath}}
         stripped_suffix=''${stripped_prefix%%.nix}
         echo "$stripped_suffix"
       done
@@ -30,7 +33,7 @@ pkgs.writeShellApplication {
     fi
 
     echo "use nix" > .envrc
-    echo "(import /etc/nixos/shells/$1.nix)" > shell.nix
+    echo "(import ${basePath}$1.nix)" > shell.nix
     direnv allow
 
     echo "Nix shell setup is complete."
