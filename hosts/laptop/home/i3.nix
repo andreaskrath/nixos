a851f1cb8a8ed67693c5d8c891cb18ac975e865b
Andreas Krath <andreas.krath+github@gmail.com>
2023-11-27 23:23:09 +0100

    laptop: add placeholder for brightness keybinds

---
+++ hosts/laptop/home/i3.nix
@@ -0,0 +1,4 @@
+#keybindings = {
+#"XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
+#"XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%+";
+#};
