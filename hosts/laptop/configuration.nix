c6d43aa8f3d8bdab0f8e19c0cf34495bdc34fb2e
Andreas Krath <andreas.krath+github@gmail.com>
2023-11-29 21:55:23 +0100

    config: add laptop config and refactor desktop into shared

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,27 +1,24 @@
-{ ... }:
+{ pkgs, config, ... }:
 {
   imports =
     [
       <home-manager/nixos>
-      /etc/nixos/hosts/desktop/hardware-configuration.nix
-      /etc/nixos/hosts/desktop/system/system.nix
+      /etc/nixos/hosts/laptop/hardware-configuration.nix
+      /etc/nixos/hosts/laptop/system/system.nix
       /etc/nixos/shared/system/system.nix
       /etc/nixos/shared/base-config.nix
     ];
-
-  fileSystems."/mnt/external" = {
-    device = "/dev/disk/by-uuid/60de9566-d001-4586-a3fd-0604a61f428d";
-    fsType = "ext4";
-  };
 
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
     users.krath = {
       imports = [
-        /etc/nixos/hosts/desktop/home/home.nix
+        /etc/nixos/hosts/laptop/home/home.nix
         /etc/nixos/shared/home/home.nix
       ];
     };
   };
+
+  system.stateVersion = "23.05";
 }
---
+++ shared/base-config.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+{
+  system.stateVersion = "23.05";
+
+  time.timeZone = "Europe/Copenhagen";
+
+  i18n = {
+    defaultLocale = "en_DK.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "da_DK.UTF-8";
+      LC_IDENTIFICATION = "da_DK.UTF-8";
+      LC_MEASUREMENT = "da_DK.UTF-8";
+      LC_MONETARY = "da_DK.UTF-8";
+      LC_NAME = "da_DK.UTF-8";
+      LC_NUMERIC = "da_DK.UTF-8";
+      LC_PAPER = "da_DK.UTF-8";
+      LC_TELEPHONE = "da_DK.UTF-8";
+      LC_TIME = "da_DK.UTF-8";
+    };
+  };
+
+  console.keyMap = "dk-latin1";
+
+  services.printing.enable = true;
+
+  environment.shells = with pkgs; [ zsh ];
+  users.defaultUserShell = pkgs.zsh;
+  users.users.krath = {
+    isNormalUser = true;
+    description = "krath";
+    extraGroups = [ "networkmanager" "wheel" "docker" "dialout" "video" ];
+  };
+  programs.zsh.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+}
