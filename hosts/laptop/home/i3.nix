256a0df260dccbdd54081d3c25a2e04323c981b9
Andreas Krath <andreas.krath+github@gmail.com>
2023-11-29 21:46:29 +0100

    laptop: add brightness keybinds
    replaces placeholder of course

--- hosts/laptop/home/i3.nix
+++ hosts/laptop/home/i3.nix
@@ -1,4 +1,11 @@
-#keybindings = {
-#"XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
-#"XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%+";
-#};
+{ pkgs, lib, ... }:
+{
+  xsession.windowManager.i3 = {
+    config = {
+      keybindings = lib.mkAfter {
+        "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
+        "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%+";
+      };
+    };
+  };
+}
