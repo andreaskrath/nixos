c85be1ffbd23fd6d103f92a505d2eb8b99f45fdf
Andreas Krath <andreas.krath+github@gmail.com>
2023-12-18 00:29:09 +0100

    laptop: add ssh git key

--- hosts/laptop/home/git.nix
+++ hosts/laptop/home/git.nix
@@ -1,6 +1,6 @@
 {
   home.file.".ssh/allowed_signers".text = ''
-    * 
+    * ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEmQFlZI9ovaJXVgNv+c3hnLcN8z9ub1pzFSV/4dUeEQ andreas.krath+github@gmail.com
   '';
 }
 
