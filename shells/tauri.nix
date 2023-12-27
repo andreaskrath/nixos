dbbccef4ef515c6b6242ebc43334b130f10451f8
Andreas Krath <andreas.krath+github@gmail.com>
2023-12-27 01:38:43 +0100

    shells: add tauri shell

---
+++ shells/tauri.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; } }:
+let
+  rust = pkgs.rust-bin.stable.latest.default.override {
+    extensions = [ "rust-src" ];
+  };
+
+  libraries = with pkgs;[
+    webkitgtk
+    gtk3
+    cairo
+    gdk-pixbuf
+    glib
+    dbus
+    openssl_3
+  ];
+
+  packages = with pkgs; [
+    rust
+    pkg-config
+    dbus
+    openssl_3
+    glib
+    gtk3
+    libsoup
+    webkitgtk
+    appimagekit
+    cargo-tauri
+  ];
+ in
+pkgs.mkShell {
+  buildInputs = packages;
+
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH";
+  XDG_DATA_DIRS = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS";
+  __NV_PRIME_RENDER_OFFLOAD = 1;
+}
