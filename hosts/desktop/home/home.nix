e2ed274fdaeb6f96723dece3dbe1aaa95cb8611f
Andreas Krath <andreas.krath+github@gmail.com>
2024-01-10 18:45:57 +0100

    desktop: add custom awakened-poe-trade package

--- hosts/desktop/home/home.nix
+++ hosts/desktop/home/home.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./i3.nix
     ./i3status.nix
     ./alacritty.nix
     ./git.nix
+  ];
+
+  home.packages = with pkgs; [
+    appimage-run
+    (pkgs.callPackage ./awakened-poe-trade.nix {})
   ];
 }
