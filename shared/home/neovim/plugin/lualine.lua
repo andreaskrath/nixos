286e2ed37422bd535bccf4a5114177443471f6f2
Andreas Krath <andreas.krath+github@gmail.com>
2024-07-15 15:03:25 +0200

    nvim: add branch to lualine

--- shared/home/neovim/plugin/lualine.lua
+++ shared/home/neovim/plugin/lualine.lua
@@ -5,7 +5,7 @@ require("lualine").setup {
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "filename" },
-		lualine_c = { "diff" },
+		lualine_c = { "branch", "diff" },
 		lualine_x = { "diagnostics" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
