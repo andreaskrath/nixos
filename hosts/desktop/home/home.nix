b994ef8213899cefa88c6e7a8e462efdb25e1e8f
Andreas Krath <andreas.krath+github@gmail.com>
2024-09-04 17:01:07 +0200

    poe: remove awakened poe appimage

--- hosts/desktop/home/home.nix
+++ hosts/desktop/home/home.nix
@@ -1,13 +1,8 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./i3.nix
     ./i3status.nix
     ./alacritty.nix
     ./git.nix
-  ];
-
-  home.packages = with pkgs; [
-    appimage-run
-    (pkgs.callPackage ./awakened-poe-trade.nix {})
   ];
 }
