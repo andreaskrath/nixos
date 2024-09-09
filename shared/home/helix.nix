3ccaa36c9449fb230fc0827cd955c7130674a9ac
Andreas Krath <andreas.krath+github@gmail.com>
2024-09-09 23:27:44 +0200

    hx: add golangci-lint lsp

--- shared/home/helix.nix
+++ shared/home/helix.nix
@@ -20,6 +20,7 @@ in {
       rust-analyzer # rust lsp
       taplo # toml toolkit
       gopls # go lsp
+      golangci-lint-langserver # go linting lsp
       svelte-language-server #svelte lsp
       python312Packages.python-lsp-server # python3 lsp
       haskell-language-server # haskell lsp
