e17d4c24f2028039004b76ccf7a04b5d92423189
Andreas Krath <andreas.krath+github@gmail.com>
2024-09-17 19:57:39 +0200

    system: add eza

--- shared/system/zsh.nix
+++ shared/system/zsh.nix
@@ -12,8 +12,9 @@
 
     shellAliases = {
       xo = "xdg-open";
-      ll = "ls -l";
-      la = "ls -A";
+      l = "${pkgs.eza}/bin/eza --icons --group-directories-first --group --long --all";
+      ll = "${pkgs.eza}/bin/eza --icons --group-directories-first --group --long";
+      la = "${pkgs.eza}/bin/eza --icons --group-directories-first --group --long --all -all";
       cat = "${pkgs.bat}/bin/bat";
       ".." = "cd ../";
       "...." = "cd ../../";
