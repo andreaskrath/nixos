43d6331a6c82bc4e9235da61427512deafe21e92
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-24 16:44:21 +0200

    home: add zellij status bar with zjstatus plugin
    also renames tab name automatically whenever a program changes

--- shared/home/zellij.nix
+++ shared/home/zellij.nix
@@ -1,7 +1,82 @@
-{...}: {
+{...}: let
+  zjstatus = "https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm";
+
+  colors = {
+    base00 = "1e1e2e"; # base
+    base01 = "181825"; # mantle
+    base02 = "313244"; # surface0
+    base03 = "45475a"; # surface1
+    base04 = "585b70"; # surface2
+    base05 = "cdd6f4"; # text
+    base06 = "f5e0dc"; # rosewater
+    base07 = "b4befe"; # lavender
+    base08 = "f38ba8"; # red
+    base09 = "fab387"; # peach
+    base0A = "f9e2af"; # yellow
+    base0B = "a6e3a1"; # green
+    base0C = "94e2d5"; # teal
+    base0D = "89b4fa"; # blue
+    base0E = "cba6f7"; # mauve
+    base0F = "f2cdcd"; # flamingo
+  };
+in {
   home.file.".config/zellij/layouts/default.kdl".text = ''
     layout {
-      pane
+        default_tab_template {
+            children
+            pane size=1 borderless=true {
+                plugin location="${zjstatus}" {
+                    format_left   "{mode}#[bg=#${colors.base00}] {tabs}"
+                    format_center ""
+                    format_space  ""
+                    format_hide_on_overlength "true"
+                    format_precedence "crl"
+
+                    border_enabled  "false"
+                    border_char     "─"
+                    border_format   "#[fg=#6C7086]{char}"
+                    border_position "top"
+
+                    mode_normal        "#[bg=#${colors.base0B},fg=#${colors.base02},bold] NORMAL#[bg=#${colors.base03},fg=#${colors.base0B}]█"
+                    mode_locked        "#[bg=#${colors.base04},fg=#${colors.base02},bold] LOCKED #[bg=#${colors.base03},fg=#${colors.base04}]█"
+                    mode_resize        "#[bg=#${colors.base08},fg=#${colors.base02},bold] RESIZE#[bg=#${colors.base03},fg=#${colors.base08}]█"
+                    mode_pane          "#[bg=#${colors.base0D},fg=#${colors.base02},bold] PANE#[bg=#${colors.base03},fg=#${colors.base0D}]█"
+                    mode_tab           "#[bg=#${colors.base07},fg=#${colors.base02},bold] TAB#[bg=#${colors.base03},fg=#${colors.base07}]█"
+                    mode_scroll        "#[bg=#${colors.base0A},fg=#${colors.base02},bold] SCROLL#[bg=#${colors.base03},fg=#${colors.base0A}]█"
+                    mode_enter_search  "#[bg=#${colors.base0D},fg=#${colors.base02},bold] ENT-SEARCH#[bg=#${colors.base03},fg=#${colors.base0D}]█"
+                    mode_search        "#[bg=#${colors.base0D},fg=#${colors.base02},bold] SEARCHARCH#[bg=#${colors.base03},fg=#${colors.base0D}]█"
+                    mode_rename_tab    "#[bg=#${colors.base07},fg=#${colors.base02},bold] RENAME-TAB#[bg=#${colors.base03},fg=#${colors.base07}]█"
+                    mode_rename_pane   "#[bg=#${colors.base0D},fg=#${colors.base02},bold] RENAME-PANE#[bg=#${colors.base03},fg=#${colors.base0D}]█"
+                    mode_session       "#[bg=#${colors.base0E},fg=#${colors.base02},bold] SESSION#[bg=#${colors.base03},fg=#${colors.base0E}]█"
+                    mode_move          "#[bg=#${colors.base0F},fg=#${colors.base02},bold] MOVE#[bg=#${colors.base03},fg=#${colors.base0F}]█"
+                    mode_prompt        "#[bg=#${colors.base0D},fg=#${colors.base02},bold] PROMPT#[bg=#${colors.base03},fg=#${colors.base0D}]█"
+                    mode_tmux          "#[bg=#${colors.base09},fg=#${colors.base02},bold] TMUX#[bg=#${colors.base03},fg=#${colors.base09}]█"
+
+                    // formatting for inactive tabs
+                    tab_normal              "#[bg=#${colors.base03},fg=#${colors.base0D}]█#[bg=#${colors.base0D},fg=#${colors.base02},bold]{index} #[bg=#${colors.base02},fg=#${colors.base05},bold] {name}{floating_indicator}#[bg=#${colors.base03},fg=#${colors.base02},bold]█"
+                    tab_normal_fullscreen   "#[bg=#${colors.base03},fg=#${colors.base0D}]█#[bg=#${colors.base0D},fg=#${colors.base02},bold]{index} #[bg=#${colors.base02},fg=#${colors.base05},bold] {name}{fullscreen_indicator}#[bg=#${colors.base03},fg=#${colors.base02},bold]█"
+                    tab_normal_sync         "#[bg=#${colors.base03},fg=#${colors.base0D}]█#[bg=#${colors.base0D},fg=#${colors.base02},bold]{index} #[bg=#${colors.base02},fg=#${colors.base05},bold] {name}{sync_indicator}#[bg=#${colors.base03},fg=#${colors.base02},bold]█"
+
+                    // formatting for the current active tab
+                    tab_active              "#[bg=#${colors.base03},fg=#${colors.base09}]█#[bg=#${colors.base09},fg=#${colors.base02},bold]{index} #[bg=#${colors.base02},fg=#${colors.base05},bold] {name}{floating_indicator}#[bg=#${colors.base03},fg=#${colors.base02},bold]█"
+                    tab_active_fullscreen   "#[bg=#${colors.base03},fg=#${colors.base09}]█#[bg=#${colors.base09},fg=#${colors.base02},bold]{index} #[bg=#${colors.base02},fg=#${colors.base05},bold] {name}{fullscreen_indicator}#[bg=#${colors.base03},fg=#${colors.base02},bold]█"
+                    tab_active_sync         "#[bg=#${colors.base03},fg=#${colors.base09}]█#[bg=#${colors.base09},fg=#${colors.base02},bold]{index} #[bg=#${colors.base02},fg=#${colors.base05},bold] {name}{sync_indicator}#[bg=#${colors.base03},fg=#${colors.base02},bold]█"
+
+                    // separator between the tabs
+                    tab_separator           "#[bg=#${colors.base00}] "
+
+                    // indicators
+                    tab_sync_indicator       " "
+                    tab_fullscreen_indicator " 󰊓"
+                    tab_floating_indicator   " 󰹙"
+
+                    command_git_branch_command     "git rev-parse --abbrev-ref HEAD"
+                    command_git_branch_format      "#[fg=blue] {stdout} "
+                    command_git_branch_interval    "10"
+                    command_git_branch_rendermode  "static"
+                }
+            }
+        }
     }
   '';
 
@@ -13,6 +88,7 @@
       simplified_ui = true;
       pane_frames = false;
       theme = "gruvbox";
+      copy_on_select = true;
       plugins = {};
       ui = {
         pane_frames = {
--- shared/home/zsh.nix
+++ shared/home/zsh.nix
@@ -35,5 +35,44 @@
       ];
       theme = "kafeitu";
     };
+
+    initExtra = ''
+      function current_dir() {
+          local current_dir=$PWD
+          if [[ $current_dir == $HOME ]]; then
+              current_dir="~"
+          else
+              current_dir=''${current_dir##*/}
+          fi
+
+          echo $current_dir
+      }
+
+      function change_tab_title() {
+          local title=$1
+          command nohup zellij action rename-tab $title >/dev/null 2>&1
+      }
+
+      function set_tab_to_working_dir() {
+          local result=$?
+          local title=$(current_dir)
+          # uncomment the following to show the exit code after a failed command
+          # if [[ $result -gt 0 ]]; then
+          #     title="$title [$result]"
+          # fi
+
+          change_tab_title $title
+      }
+
+      function set_tab_to_command_line() {
+          local cmdline=$1
+          change_tab_title $cmdline
+      }
+
+      if [[ -n $ZELLIJ ]]; then
+          add-zsh-hook precmd set_tab_to_working_dir
+          add-zsh-hook preexec set_tab_to_command_line
+      fi
+    '';
   };
 }
