9a003219d7bc096d8443811ce6bdca1c459ed576
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-26 18:37:30 +0200

    util: begin work on mkHomeManager function

--- make.nix
+++ make.nix
@@ -1,4 +1,28 @@
 {inputs, ...}: let
+  # might get this working in the future
+  # mkHomeManager = {
+  #   username,
+  #   homeDirectory ? "/home/${username}",
+  #   system ? "x86_64-linux",
+  # }: let
+  #   pkgs = import inputs.nixpkgs {
+  #     inherit system;
+  #     config.allowUnfree = true;
+  #   };
+  # in
+  #   inputs.home-manager.lib.homeManagerConfiguration {
+  #     inherit pkgs inputs;
+  #     modules = [
+  #       {
+  #         home = {
+  #           inherit username homeDirectory;
+  #           useGlobalPkgs = true;
+  #           useUserPackages = true;
+  #         };
+  #         programs.home-manager.enable = true;
+  #       }
+  #     ];
+  #   };
   mkNixOS = {
     hostname,
     system ? "x86_64-linux",
