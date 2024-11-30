c365332de44a5bfa81551b3b5e5abb4e69bad729
Andreas Krath <andreas.krath+github@gmail.com>
2024-11-30 16:46:44 +0100

    inputs: disable helix

--- flake.nix
+++ flake.nix
@@ -11,10 +11,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    helix-master = {
-      url = "github:helix-editor/helix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # helix-master = {
+    #   url = "github:helix-editor/helix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     zjstatus = {
       url = "github:dj95/zjstatus";
