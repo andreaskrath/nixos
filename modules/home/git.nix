6ef80df2a0620a2753a122465d1c536df359dccb
Andreas Krath <andreas.krath+github@gmail.com>
2025-02-15 01:42:17 +0100

    home: refactor to new git signing format

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,10 +3,10 @@
     enable = true;
     userName = "Andreas Krath";
     userEmail = "andreas.krath+github@gmail.com";
+    signing.format = "ssh";
 
     extraConfig = {
       commit.gpgsign = true; # sign all commits using ssh key
-      gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       user.signingkey = "~/.ssh/id_ed25519.pub";
     };
