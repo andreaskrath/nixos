84dca2ec1dc1b1ff38b6bd019d1169da33181b03
Andreas Krath <andreas.krath+github@gmail.com>
2025-03-14 17:10:14 +0100

    Chiefs: Bumps greeter module version

--- hosts/chiefs/greeter.nix
+++ hosts/chiefs/greeter.nix
@@ -5,12 +5,12 @@
 }:
 buildGoModule rec {
   pname = "greeter";
-  version = "0.2.0";
+  version = "0.3.1";
   src = fetchFromGitHub {
     owner = "thegrubster";
     repo = pname;
     rev = "${version}";
-    sha256 = "sha256-+wRZbmUe7JkRbpBlXw6Fqe73Z+AsP6QkHEmtQ8BRNGg=";
+    sha256 = "sha256-MLseNtWTZRgzmo2XdY2EQ10k5vb6dkODxGPXeBuD1pw=";
   };
 
   vendorHash = "sha256-+tEmXHlvG6R4NAXBoVbYpPe7hrF77BV5vfeFI0PaYxA=";
