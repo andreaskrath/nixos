d7635e0deca0bc8435b61ef1d61a92d1170df4de
Andreas Krath <andreas.krath+github@gmail.com>
2025-04-22 18:32:21 +0200

    i3Status: Add external drive on `mozart` to bar

--- hosts/mozart/home/i3status.nix
+++ hosts/mozart/home/i3status.nix
@@ -3,5 +3,12 @@
     "cpu_temperature 0" = {
       settings.path = "/sys/devices/platform/coretemp.0/hwmon*/hwmon*/temp1_input";
     };
+    "disk /mnt/external" = {
+      enable = true;
+      position = 51;
+      settings = {
+        format = "EXTERNAL %free";
+      };
+    };
   };
 }
