3058ee0a4e8cdb88c93e85d0ecd96e92e005f987
Andreas Krath <andreas.krath+github@gmail.com>
2025-05-05 10:21:48 +0200

    Chiefs: Correctly update greeter module to new version
    Forgot to update hashes when rebuilding, so it just built the old.

--- hosts/chiefs/greeter.nix
+++ hosts/chiefs/greeter.nix
@@ -5,15 +5,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "greeter";
+
+  # Remember to clear hashes upon version bumping
   version = "1.0.2";
 
   src = fetchFromGitHub {
     owner = "andreaskrath";
     repo = pname;
     rev = version;
-    sha256 = "sha256-VeQlkXSIehgZdt0svhaCi2Z4uUe+gp6NBNW6K9jIljA=";
+    sha256 = "sha256-ZDorl+4u4Y4Hj4jsj5yWJ9y92B56LmZCMztZYEooQ7A=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-lK2Z6bjnVymYqWmR2t5AANsrnowWtK94XRmv90xFzu4=";
+  cargoHash = "sha256-8d1tpKg+QnSTy9Kuf7Q6DCUWMDSIBJS87uEoMbvwrSY=";
 }
