816cb58388b6891ffa2ab0832c801f1d252cffe7
Andreas Krath <andreas.krath+github@gmail.com>
2025-05-05 10:37:33 +0200

    Chiefs: Bump greeter module version

--- hosts/chiefs/greeter.nix
+++ hosts/chiefs/greeter.nix
@@ -7,15 +7,15 @@ rustPlatform.buildRustPackage rec {
   pname = "greeter";
 
   # Remember to clear hashes upon version bumping
-  version = "1.0.2";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "andreaskrath";
     repo = pname;
     rev = version;
-    sha256 = "sha256-ZDorl+4u4Y4Hj4jsj5yWJ9y92B56LmZCMztZYEooQ7A=";
+    sha256 = "sha256-4pA5Xr/rCOVa0+S4DK+TlkI5+V+IWZ4WEQgOoD+U5GU=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-8d1tpKg+QnSTy9Kuf7Q6DCUWMDSIBJS87uEoMbvwrSY=";
+  cargoHash = "sha256-9j/1S8gJK7UkpEneInIj0zW+Xj0rSeG4YjkZLnmOj7k=";
 }
