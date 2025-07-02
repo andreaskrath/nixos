f365a1f6d6081a1906472299d4687dcbbc681ac5
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-02 22:46:56 +0200

    Base: Remove beekeeper permit

--- modules/base-config.nix
+++ modules/base-config.nix
@@ -39,8 +39,4 @@
   environment.variables.SUDO_EDITOR = lib.mkOverride 900 "nvim";
 
   nix.settings.auto-optimise-store = true;
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "beekeeper-studio-5.1.5"
-  ];
 }
