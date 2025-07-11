5f4653b54dbb455cbc9e615246e9b3d91fad9db8
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-11 23:31:24 +0200

    Chopin: Apply new style for Polybar
    Configuration might be slightly off, have not test driven it yet - but
    it worked flawlessly on mozart.

--- hosts/chopin/home/home.nix
+++ hosts/chopin/home/home.nix
@@ -1,11 +1,32 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  homeModules,
+  ...
+}: {
   imports = [
     ./i3.nix
     ./git.nix
+    "${homeModules}/polybar.nix"
   ];
 
   home.packages = with pkgs; [
     (pkgs.callPackage ./monitor-script.nix {})
     libreoffice
   ];
+
+  krath = {
+    polybar = {
+      enable = true;
+      enableBattery = true;
+
+      monitors = ["eDP"];
+      fileSystems = ["/"];
+
+      primaryBarWidth = "97.8%";
+      xOffset = "1.1%";
+
+      thermalZone = 0;
+      thermalType = "acpitz";
+    };
+  };
 }
