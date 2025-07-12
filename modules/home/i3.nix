9a1980080a6b8c4b11f27dc8a7ab1888d967c294
Andreas Krath <andreas.krath+github@gmail.com>
2025-07-12 10:41:08 +0200

    Refactor: Start work on getting i3 to new module format
    I really suck at Nix though.

--- modules/home/i3.nix
+++ modules/home/i3.nix
@@ -1,5 +1,13 @@
-{pkgs, ...}: let
-  # workspaces
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cfg = config.krath.home.i3;
+
+  bindWorkspaceToOutput = output: workspace: "workspace ${workspace} output ${output}";
+
   ws1 = "1";
   ws2 = "2";
   ws3 = "3";
@@ -11,119 +19,186 @@
   ws9 = "9";
   ws10 = "10";
 in {
-  xsession = {
-    enable = true;
-    windowManager.i3 = {
-      enable = true;
+  options.krath.home.i3 = {
+    enable = lib.mkEnableOption "Enable i3 module.";
 
-      config = rec {
-        modifier = "Mod4";
-        defaultWorkspace = "workspace ${ws1}";
-        focus = {
-          followMouse = false;
-          mouseWarping = false;
-        };
-        floating.titlebar = false;
-        terminal = "alacritty";
-
-        startup = [
-          {
-            command = "systemctl --user restart polybar";
-            always = true;
-            notification = false;
-          }
-        ];
-
-        bars = [];
-
-        assigns = {
-          "${ws1}" = [{class = "^Navigator$";} {class = "^brave-browser$";} {class = "^Brave-browser$";}];
-          "${ws2}" = [{class = "^Alacritty$";}];
-          "${ws6}" = [{class = "^obsidian$";}];
-          "${ws9}" = [{class = "^discord$";}];
-          "${ws10}" = [{class = "^spotify$";}];
-        };
-
-        window = {
-          border = 1;
-          titlebar = false;
-        };
-
-        gaps = {
-          inner = 15;
-          outer = 5;
-          top = 40;
-        };
-
-        keybindings = {
-          "${modifier}+1" = "workspace ${ws1}";
-          "${modifier}+2" = "workspace ${ws2}";
-          "${modifier}+3" = "workspace ${ws3}";
-          "${modifier}+4" = "workspace ${ws4}";
-          "${modifier}+5" = "workspace ${ws5}";
-          "${modifier}+6" = "workspace ${ws6}";
-          "${modifier}+7" = "workspace ${ws7}";
-          "${modifier}+8" = "workspace ${ws8}";
-          "${modifier}+9" = "workspace ${ws9}";
-          "${modifier}+0" = "workspace ${ws10}";
-
-          "${modifier}+Shift+1" = "move container to workspace ${ws1}";
-          "${modifier}+Shift+2" = "move container to workspace ${ws2}";
-          "${modifier}+Shift+3" = "move container to workspace ${ws3}";
-          "${modifier}+Shift+4" = "move container to workspace ${ws4}";
-          "${modifier}+Shift+5" = "move container to workspace ${ws5}";
-          "${modifier}+Shift+6" = "move container to workspace ${ws6}";
-          "${modifier}+Shift+7" = "move container to workspace ${ws7}";
-          "${modifier}+Shift+8" = "move container to workspace ${ws8}";
-          "${modifier}+Shift+9" = "move container to workspace ${ws9}";
-          "${modifier}+Shift+0" = "move container to workspace ${ws10}";
-
-          "${modifier}+Shift+q" = "kill";
-
-          "${modifier}+h" = "focus left";
-          "${modifier}+j" = "focus down";
-          "${modifier}+k" = "focus up";
-          "${modifier}+l" = "focus right";
-
-          "${modifier}+Shift+h" = "move left";
-          "${modifier}+Shift+j" = "move down";
-          "${modifier}+Shift+k" = "move up";
-          "${modifier}+Shift+l" = "move right";
-
-          "${modifier}+v" = "split v";
-          "${modifier}+f" = "fullscreen toggle";
-
-          "${modifier}+s" = "layout stacking";
-          "${modifier}+w" = "layout tabbed";
-          "${modifier}+e" = "layout toggle split";
-
-          "${modifier}+Shift+space" = "floating toggle";
-          "${modifier}+space" = "focus mode_toggle";
-
-          "${modifier}+a" = "focus parent";
-
-          "${modifier}+Shift+minus" = "move scratchpad";
-          "${modifier}+minus" = "scratchpad show";
-
-          "${modifier}+Shift+c" = "reload";
-          "${modifier}+Shift+r" = "restart";
-          "${modifier}+Shift+e" = "exec i3-nagbar -t warning -m 'Do you want to exit i3?' -b 'Yes' 'i3-msg exit'";
-
-          "${modifier}+r" = "mode resize";
-          "XF86AudioMute" = "exec ${pkgs.alsa-utils}/bin/amixer set Master toggle";
-          "XF86AudioLowerVolume" = "exec ${pkgs.alsa-utils}/bin/amixer set Master 5%-";
-          "XF86AudioRaiseVolume" = "exec ${pkgs.alsa-utils}/bin/amixer set Master 5%+";
-          "${modifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
-          "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show run";
-          "${modifier}+Shift+d" = "exec ${pkgs.rofi}/bin/rofi -show window";
-          "${modifier}+b" = "exec ${pkgs.brave}/bin/brave";
-          "${modifier}+Shift+s" = "exec ${pkgs.maim}/bin/maim -s | ${pkgs.xclip}/bin/xclip -selection clipboard -t image/png -i";
-        };
-      };
-
-      extraConfig = ''
-        for_window [class="Spotify"] move to workspace ${ws10}
+    extraAssigns = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.listOf (lib.types.attrsOf (lib.types.str)));
+      default = {};
+      description = ''
+        Extra assignments of application classes to specific workspaces.
       '';
+    };
+
+    extraStartup = lib.mkOption {
+      type = lib.types.listOf (lib.types.attrsOf (lib.types.str lib.types.bool));
+      default = [];
+      description = ''
+        Extra startup commands to run.
+      '';
+    };
+
+    extraKeybinds = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = {};
+      description = ''
+        Extra keybinds to add.
+      '';
+    };
+
+    bindWorkspaces = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.listOf (lib.types.str));
+      default = {};
+      description = ''
+        Bind specific workspaces to specific outputs.
+
+        The outputs in question should be monitor ports, like DP-1 etc.
+      '';
+      example = lib.literalExpression ''
+        {
+          DP-2 = ["1" "2" "3"];
+          DP-4 = ["4" "5"];
+        }
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    xsession = {
+      enable = true;
+      windowManager.i3 = {
+        enable = true;
+
+        config = rec {
+          modifier = "Mod4";
+          defaultWorkspace = "workspace ${ws1}";
+          focus = {
+            followMouse = false;
+            mouseWarping = false;
+          };
+          floating.titlebar = false;
+          terminal = "alacritty";
+
+          startup =
+            [
+              {
+                command = "systemctl --user restart polybar";
+                always = true;
+                notification = false;
+              }
+            ]
+            ++ cfg.extraStartup;
+
+          bars = [];
+
+          assigns = lib.mkMerge [
+            {
+              "${ws1}" = [{class = "^Navigator$";} {class = "^brave-browser$";} {class = "^Brave-browser$";}];
+              "${ws2}" = [{class = "^Alacritty$";}];
+              "${ws6}" = [{class = "^obsidian$";}];
+              "${ws9}" = [{class = "^discord$";}];
+              "${ws10}" = [{class = "^spotify$";}];
+            }
+            cfg.extraAssigns
+          ];
+
+          window = {
+            border = 1;
+            titlebar = false;
+          };
+
+          gaps = {
+            inner = 15;
+            outer = 5;
+            top = 40;
+          };
+
+          keybindings = lib.mkMerge [
+            {
+              "${modifier}+1" = "workspace ${ws1}";
+              "${modifier}+2" = "workspace ${ws2}";
+              "${modifier}+3" = "workspace ${ws3}";
+              "${modifier}+4" = "workspace ${ws4}";
+              "${modifier}+5" = "workspace ${ws5}";
+              "${modifier}+6" = "workspace ${ws6}";
+              "${modifier}+7" = "workspace ${ws7}";
+              "${modifier}+8" = "workspace ${ws8}";
+              "${modifier}+9" = "workspace ${ws9}";
+              "${modifier}+0" = "workspace ${ws10}";
+
+              "${modifier}+Shift+1" = "move container to workspace ${ws1}";
+              "${modifier}+Shift+2" = "move container to workspace ${ws2}";
+              "${modifier}+Shift+3" = "move container to workspace ${ws3}";
+              "${modifier}+Shift+4" = "move container to workspace ${ws4}";
+              "${modifier}+Shift+5" = "move container to workspace ${ws5}";
+              "${modifier}+Shift+6" = "move container to workspace ${ws6}";
+              "${modifier}+Shift+7" = "move container to workspace ${ws7}";
+              "${modifier}+Shift+8" = "move container to workspace ${ws8}";
+              "${modifier}+Shift+9" = "move container to workspace ${ws9}";
+              "${modifier}+Shift+0" = "move container to workspace ${ws10}";
+
+              "${modifier}+Shift+q" = "kill";
+
+              "${modifier}+h" = "focus left";
+              "${modifier}+j" = "focus down";
+              "${modifier}+k" = "focus up";
+              "${modifier}+l" = "focus right";
+
+              "${modifier}+Shift+h" = "move left";
+              "${modifier}+Shift+j" = "move down";
+              "${modifier}+Shift+k" = "move up";
+              "${modifier}+Shift+l" = "move right";
+
+              "${modifier}+v" = "split v";
+              "${modifier}+f" = "fullscreen toggle";
+
+              "${modifier}+s" = "layout stacking";
+              "${modifier}+w" = "layout tabbed";
+              "${modifier}+e" = "layout toggle split";
+
+              "${modifier}+Shift+space" = "floating toggle";
+              "${modifier}+space" = "focus mode_toggle";
+
+              "${modifier}+a" = "focus parent";
+
+              "${modifier}+Shift+minus" = "move scratchpad";
+              "${modifier}+minus" = "scratchpad show";
+
+              "${modifier}+Shift+c" = "reload";
+              "${modifier}+Shift+r" = "restart";
+              "${modifier}+Shift+e" = "exec i3-nagbar -t warning -m 'Do you want to exit i3?' -b 'Yes' 'i3-msg exit'";
+
+              "${modifier}+r" = "mode resize";
+              "XF86AudioMute" = "exec ${pkgs.alsa-utils}/bin/amixer set Master toggle";
+              "XF86AudioLowerVolume" = "exec ${pkgs.alsa-utils}/bin/amixer set Master 5%-";
+              "XF86AudioRaiseVolume" = "exec ${pkgs.alsa-utils}/bin/amixer set Master 5%+";
+              "${modifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
+              "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show run";
+              "${modifier}+Shift+d" = "exec ${pkgs.rofi}/bin/rofi -show window";
+              "${modifier}+b" = "exec ${pkgs.brave}/bin/brave";
+              "${modifier}+Shift+s" = "exec ${pkgs.maim}/bin/maim -s | ${pkgs.xclip}/bin/xclip -selection clipboard -t image/png -i";
+            }
+            cfg.extraKeybinds
+          ];
+        };
+
+        extraConfig =
+          ''
+            for_window [class="Spotify"] move to workspace ${ws10}
+          ''
+          + builtins.concatStringsSep "\n" (
+            lib.flatten (
+              lib.mapAttrsToList (
+                output: workspaces:
+                  lib.map (
+                    workspace: bindWorkspaceToOutput output workspace
+                  )
+                  workspaces
+              )
+              cfg.bindWorkspaces
+            )
+          );
+      };
     };
   };
 }
