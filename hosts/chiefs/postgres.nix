ea2a6e4f0511f60388177b4b2d9e4ad96cb82a5b
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-03 20:48:20 +0100

    Chief/Postgres: Define access entries for localhost connections

--- hosts/chiefs/postgres.nix
+++ hosts/chiefs/postgres.nix
@@ -4,9 +4,11 @@
     enableTCPIP = true;
     ensureDatabases = ["dnd"];
     authentication = pkgs.lib.mkOverride 10 ''
-      # type  database user_name address   auth_method
-        local all      all                 trust
-        host  all      all       0.0.0.0/0 scram-sha-256
+      # type  database user_name address         auth_method
+      local   all      all                       trust
+      host    all      all        127.0.0.1/32   trust
+      host    all      all        ::1/128        trust
+      host    all      all        0.0.0.0/0      scram-sha-256
     '';
 
     settings = {
