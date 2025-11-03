a7ef1c907718932a992e8a47610126d1a1abbcd8
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-03 21:13:44 +0100

    Flake: Update all inputs, and fix zjstatus issue with oxalica overlay
    The issue is upstream. I opened an issue, but it has not been fixed as
    of now.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,6 +17,7 @@
     zjstatus = {
       url = "github:dj95/zjstatus";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
     };
 
     stylix = {
