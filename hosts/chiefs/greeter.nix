8528d9335aa0b11d6b53643d870ce50bcd84aa74
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-03 20:45:36 +0100

    Chiefs: Remove greeter options that caused warning
    Something about being deprecated.

--- hosts/chiefs/greeter.nix
+++ hosts/chiefs/greeter.nix
@@ -16,6 +16,5 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-4pA5Xr/rCOVa0+S4DK+TlkI5+V+IWZ4WEQgOoD+U5GU=";
   };
 
-  useFetchCargoVendor = true;
   cargoHash = "sha256-9j/1S8gJK7UkpEneInIj0zW+Xj0rSeG4YjkZLnmOj7k=";
 }
