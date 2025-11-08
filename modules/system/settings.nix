31d917666dd8c86cee5058d0294bbfcbd25c72f5
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-08 22:03:34 +0100

    Settings: Add `nixpkgs-python` cachix

--- modules/system/settings.nix
+++ modules/system/settings.nix
@@ -11,11 +11,13 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
+        "https://nixpkgs-python.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+        "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
       ];
     };
   };
