28cb30393cb25ca0c4b48db4b81c899ebd0f8fe6
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-08 22:04:06 +0100

    Flake: Set zjstatus input to fork with upstream fix
    Hopefully the fix is merged soon.

--- flake.nix
+++ flake.nix
@@ -4,20 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     zjstatus = {
-      url = "github:dj95/zjstatus";
+      url = "github:andreaskrath/zjstatus/fix-lock-file";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rust-overlay.follows = "rust-overlay";
     };
 
     stylix = {
