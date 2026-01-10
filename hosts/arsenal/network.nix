9f8b8e763e3feb1ee1e91738f8a1b0becc36fdab
Andreas Krath <andreas.krath+github@gmail.com>
2026-01-10 23:27:27 +0100

    arsenal: enable mullvad-vpn service
    there are no options to manage it, but options can be set via CLI or via
    a settings.json file the latter already having a default config on
    init, which is an issue
    
    if i change anything at any point, it will likely be a systemd service
    to ensure vpc activates upon launch and so on - via CLi commands

--- hosts/arsenal/network.nix
+++ hosts/arsenal/network.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services.openssh = {
     enable = true;
     openFirewall = true;
@@ -14,6 +14,11 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNjyEbOjbtKjrVjzEZo+3WWm/Z1TECQ4u80cRPnCv3W andreas.krath+github@gmail.com" # desktop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEmQFlZI9ovaJXVgNv+c3hnLcN8z9ub1pzFSV/4dUeEQ andreas.krath+github@gmail.com" # laptop
     ];
+  };
+
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad;
   };
 
   # networking.firewall.allowedTCPPorts = [80 443 5432];
