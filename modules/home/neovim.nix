73fcf0e0a9ef3aa72b6eb4827eac1d7caa84bdb1
Andreas Krath <andreas.krath+github@gmail.com>
2026-05-11 13:22:43 +0200

    home/neovim: drop using home manager
    idk, i just cannot be bothered dealing with upstream people changing
    their mind on what files they generate all the time

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -2,27 +2,36 @@
   config,
   inputs,
   pkgs,
+  lib,
   ...
-}: {
-  stylix.targets.neovim.enable = false;
+}: let
+  packages = with pkgs; [
+    ripgrep
+    xclip
+    lua-language-server
+    taplo
+    nil
+    alejandra
+    clang
+  ];
 
-  programs.neovim = {
-    enable = true;
-    package = inputs.neovim.legacyPackages.${pkgs.stdenv.hostPlatform.system}.neovim-unwrapped;
-    viAlias = true;
-    vimAlias = true;
-    defaultEditor = true;
+  # Stopped using home manager neovim package because it often changes internals that
+  # generated more random files causing weird interactions in my config.
+  neovim =
+    pkgs.wrapNeovim
+    inputs.neovim.legacyPackages.${pkgs.stdenv.hostPlatform.system}.neovim-unwrapped
+    {
+      viAlias = true;
+      vimAlias = true;
+      extraMakeWrapperArgs = "--prefix PATH : ${lib.makeBinPath packages}";
+    };
+in {
+  home.packages = [neovim];
 
-    extraPackages = with pkgs; [
-      ripgrep
-      xclip
-      lua-language-server
-      taplo
-      nil
-      alejandra
-      clang # For treesitter grammars
-    ];
-  };
-
+  # Whenever an error is `Error installing file '<file-path>' outside $HOME`,
+  # it is incredibly likely the underlying issue is because something else is
+  # trying to place a file at the same file path, this could be home manager, stylix or whatever.
+  #
+  # The error message itself is just horrendous.
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink /etc/nixos/nvim;
 }
